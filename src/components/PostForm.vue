ae664d81540cbc54fe41ff4f54c19c8aa8cc73db
Serhii Moskalov <sv.moskalyov@gmail.com>
2023-12-09 11:35:23 +0200

    start project one

---
+++ src/components/PostForm.vue
@@ -0,0 +1,63 @@
+<template>
+  <form @submit.prevent>
+    <h3>Created post</h3>
+    <input
+      v-bind:value="post.title"
+      @input="post.title = $event.target.value"
+      class="input"
+      type="text"
+      placeholder="Name"
+    />
+    <input
+      v-bind:value="post.body"
+      @input="post.body = $event.target.value"
+      class="input"
+      type="text"
+      placeholder="Description"
+    />
+    <button class="btn">Add post</button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "PostForm",
+  data() {
+    return {
+      post: {
+        title: "",
+        body: "",
+      },
+    };
+  },
+};
+</script>
+
+<style scoped>
+form {
+  display: flex;
+  flex-direction: column;
+}
+
+.input {
+  width: 100%;
+  border: 1px solid tomato;
+  padding: 10px 15px;
+  margin-top: 15px;
+}
+
+.btn {
+  margin-top: 15px;
+  align-self: flex-end;
+  padding: 10px 15px;
+  background: none;
+  color: tomato;
+  border: 1px solid tomato;
+  cursor: pointer;
+
+  &:hover {
+    background: tomato;
+    color: #fff;
+  }
+}
+</style>
---
+++ src/components/PostList.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="post" v-for="post in posts">
+    <div><strong>Name: </strong> {{ post.title }}</div>
+    <div><strong>Description: </strong> {{ post.body }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PostList",
+  props: {
+    posts: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.post {
+  padding: 15px;
+  border: 2px solid tomato;
+  margin-top: 15px;
+}
+</style>
