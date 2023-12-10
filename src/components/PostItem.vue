467388ebff8b7d6dd80a738a52dff80f62492396
Serhii Moskalov <sv.moskalyov@gmail.com>
2023-12-10 12:23:55 +0200

    Update PostItem.vue

--- src/components/PostItem.vue
+++ src/components/PostItem.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="post">
     <div>
-      <div><strong>Name: </strong> {{ post.title }}</div>
+      <div><strong>Title: </strong> {{ post.title }}</div>
       <div><strong>Description: </strong> {{ post.body }}</div>
     </div>
     <div class="post__btns">
