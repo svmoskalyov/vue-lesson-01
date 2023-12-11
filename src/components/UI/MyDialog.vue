7842901a0c8ba615fbdf824fdc0cf0e136650b41
Serhii Moskalov <sv.moskalyov@gmail.com>
2023-12-11 17:31:07 +0200

    added mixins

--- src/components/UI/MyDialog.vue
+++ src/components/UI/MyDialog.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="dialog" v-if="show" @click.stop="hideDialog" >
+  <div class="dialog" v-if="show" @click.stop="hideDialog">
     <div @click.stop class="dialog__content">
       <slot></slot>
     </div>
@@ -7,19 +7,25 @@
 </template>
 
 <script>
+import toggleMixin from "@/mixins/toggleMixin";
+
 export default {
   name: "my-dialog",
-  props: {
-    show: {
-      type: Boolean,
-      default: false,
-    },
-    },
-    methods: {
-        hideDialog() {
-        this.$emit('update:show', false)
-    }
-  }
+  // props: {
+  //   show: {
+  //     type: Boolean,
+  //     default: false,
+  //   },
+  //   },
+  //   methods: {
+  //       hideDialog() {
+  //       this.$emit('update:show', false)
+  //   }
+  // }
+  mixins: [toggleMixin],
+  // mounted() {
+  //   console.log("dialog mounted");
+  // },
 };
 </script>
 
