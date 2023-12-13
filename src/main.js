9ca2ef3e82f30ab62d6e376997fbbb0be359ea17
Serhii Moskalov <sv.moskalyov@gmail.com>
2023-12-13 16:49:04 +0200

    added  pinia

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import { createApp } from "vue";
+import { createPinia } from "pinia";
 import App from "./App.vue";
 import components from "@/components/UI";
 import router from "@/router";
 import directives from "@/directives";
 import store from "@/store";
 
+const pinia = createPinia();
 const app = createApp(App);
 
 components.forEach((component) => {
@@ -15,4 +17,5 @@ directives.forEach((directive) => {
   app.directive(directive.name, directive);
 });
 
-app.use(router).use(store).mount("#app");
+// app.use(router).use(store).mount("#app");
+app.use(router).use(store).use(pinia).mount("#app");
